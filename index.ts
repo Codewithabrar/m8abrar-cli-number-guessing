f33e8ee773c998d7d0e98f58135004852f8474c9
Codewithabrar <162000914+Codewithabrar@users.noreply.github.com>
2024-04-21 18:32:23 +0500

    this is number guessing game

---
+++ index.ts
@@ -0,0 +1,13 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()*6+1);
+const answers = await inquirer.prompt([{
+    name: "userRandomnumber",
+    type: "number",
+    message: "Please select a number between 1-6"
+}])
+if(answers.userRandomnumber === randomNumber){
+    console.log("congartulation your number is correct");;
+}else{
+    console.log("op's your number is wromng");
+}
