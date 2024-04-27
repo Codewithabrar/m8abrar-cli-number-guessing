7622f8d7114ca7e82924034e7c327047439869da
Codewithabrar <162000914+Codewithabrar@users.noreply.github.com>
2024-04-27 11:58:31 +0500

    Coloring code

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random()*6+1);
 const answers = await inquirer.prompt([{
     name: "userRandomnumber",
     type: "number",
-    message: "Please select a number between 1-6"
+    message: `${chalk.red.bold("Please select a number between 1-6")}`
 }])
 if(answers.userRandomnumber === randomNumber){
-    console.log("congartulation your number is correct");
+    console.log(`${chalk.green.bold("congartulation your number is correct")}`);
 }else{
-    console.log("op's your number is wromng");
+    console.log(`${chalk.cyan.bold("op's your number is wromng")}`);
 }
